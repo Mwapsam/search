75cea8dbd266365ea23b27f14f6aadbf258a4685
Mwapsam <mwapsam@gmail.com>
2021-12-27 07:15:46 +0200

    configure linter errors

---
+++ README.md
@@ -0,0 +1,52 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Module 1 - Capstone Project
+
+> Building my first Capstone project to apply the concepts learned in order to enhance my skills in HTML, CSS & JavaScript.
+
+![screenshot](./img/Screenshot.png)
+
+
+
+Additional description about the project and its features.
+
+## Built With
+
+- Major languages
+- HTML
+- JavaScript
+- CSS
+
+## Live Demo
+
+[Live Demo Link](https://mwapsam.github.io/search/)
+
+
+## Getting Started
+
+**To get a copy of this repo into your local machine run the following command:**
+
+```
+git clone https://github.com/Mwapsam/capstone-m1
+cd capstone-m1
+in visual studio code you can click the go live button to view it live in the browser.
+```
+
+## Author
+
+👤 **Samuel Chimfwembe**
+
+- GitHub: [@Mwapsam](https://github.com/Mwapsam)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- search.js
+++ search.js
@@ -1,20 +1,21 @@
+// eslint-disable-next-line no-unused-vars
 const search = () => {
-    const searchBox = document.getElementById('search-item').value.toUpperCase();
-    const storeItems = document.getElementById('product-list');
-    const product = document.querySelectorAll('.product');
-    const pname = storeItems.getElementsByTagName('h2');
-    
-    for (let j = 0; j < pname.length; j++) {
-      let match = product[j].getElementsByTagName('h2')[0];
-  
-      if (match) {
-        let textValue = match.textContent || match.innerHTML;
-  
-        if (textValue.toUpperCase().indexOf(searchBox) > -1) {
-          product[j].style.display = '';
-        } else {
-          product[j].style.display = 'none';
-        }
+  const searchBox = document.getElementById('search-item').value.toUpperCase();
+  const storeItems = document.getElementById('product-list');
+  const product = document.querySelectorAll('.product');
+  const pname = storeItems.getElementsByTagName('h2');
+
+  for (let j = 0; j < pname.length; j += 1) {
+    const match = product[j].getElementsByTagName('h2')[0];
+
+    if (match) {
+      const textValue = match.textContent || match.innerHTML;
+
+      if (textValue.toUpperCase().indexOf(searchBox) > -1) {
+        product[j].style.display = '';
+      } else {
+        product[j].style.display = 'none';
       }
     }
-  };
+  }
+};
